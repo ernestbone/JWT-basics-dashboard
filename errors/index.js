0fa6b348c7cf94ae99cfeb2d0cb8e0111fcedaa4
ernestbone <owhiroroernest@gmail.com>
2022-08-24 11:39:22 +0100

    initial commit

---
+++ errors/index.js
@@ -0,0 +1,10 @@
+const customAPIError = require('./custom-error')
+const BadRequestError = require('./bad-request')
+const UnauthenticatedError = require('./unauthenticated')
+
+
+module.exports = {
+    customAPIError,
+    BadRequestError,
+    UnauthenticatedError,
+}
